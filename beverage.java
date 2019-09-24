e0b8798f5714a6f121839008ae9277aeb7ac18ed
blackmanjack <55394422+blackmanjack@users.noreply.github.com>
2019-09-24 07:22:26 +0700

    minuman

---
+++ beverage.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class beverage extends AppCompatActivity {
+
+    public static void setOnClickListener(MoveWithDataActivity moveWithDataActivity) {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beverage);
+    }
+}
